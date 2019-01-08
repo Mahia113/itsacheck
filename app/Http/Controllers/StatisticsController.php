ca9cbe45cf3936f3e1252c7e0f32186c94d678c6
Jose Luis <joselopez@anzen.com.mx>
2019-01-08 12:43:31 -0600

    Add the first functions and design of statics functions

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Carrer;
+use App\Group;
 use App\NoAssistance;
 use App\Profesor;
+use App\Subject;
 use Illuminate\Http\Request;
 
 class StatisticsController extends Controller
@@ -19,7 +22,11 @@ class StatisticsController extends Controller
             ->with('faults', $this->getTotalFaults())
             ->with('totalRowsInNoAssistance', $this->getTotalRowsInNoAssistance())
             ->with('totalProfesorsWithFaults', $this->getTotalProfesorsWithFaults())
-            ->with('totalProfesors', $this->getTotalProfesors());
+            ->with('totalProfesors', $this->getTotalProfesors())
+            ->with('totalGroupsWithFaults', $this->getTotalGroupsWithFaults())
+            ->with('totalGroups', $this->getTotalGroups())
+            ->with('carrerWithMoreFaults', $this->getCarrerWithMoreFaults())
+            ->with('profesorWithMoreFaultsByCarrer', $this->getProfesorWithMoreFaultsByCarrer());
     }
 
     public function getTotalFaults(){
@@ -47,8 +54,117 @@ class StatisticsController extends Controller
     }
 
     public function getTotalProfesors(){
-        return Profesor::all()
-            ->count();
+        return Profesor::all()->count();
     }
 
+    public function getTotalGroupsWithFaults(){
+        $subjects = NoAssistance::select('subject_id')
+            ->where('assistance', false)
+            ->groupBy('subject_id')
+            ->get();
+
+        return $this->getGroups($subjects);
+    }
+
+    public function getGroups($subjects){
+
+        $total = 0;
+        $group = null;
+
+        foreach ($subjects as $subject){
+
+            $total += $group = Group::select('id')
+                ->where('id', $subject->subject_id)
+                ->groupBy('id')
+                ->count();
+        }
+
+        return $total;
+    }
+
+    public function getTotalGroups(){
+        return Group::all()->count();
+    }
+
+    public function getCarrerWithMoreFaults(){
+
+        $ISIC = 0; $IINF = 0; $IBQA = 0; $ICIV = 0; $IGEM = 0; $IIAS = 0; $IIND = 0; $COP = 0;
+
+        $subjects = NoAssistance::select('subject_id')
+            ->where('assistance', false)
+            ->get();
+
+        foreach ($subjects as $subject => $value){
+                $subjectsIDs[] = "$value->subject_id";
+        }
+
+        $carrers = Subject::select('carrer_id')
+            ->whereIn('id', $subjectsIDs)
+            ->get();
+
+        foreach ($carrers as $carrer){
+
+            switch($carrer->carrer_id){
+                case 1:
+                    $ISIC += 1;
+                    break;
+                case 2:
+                    $IINF += 1;
+                    break;
+                case 3:
+                    $IBQA += 1;
+                    break;
+                case 4:
+                    $ICIV += 1;
+                    break;
+                case 5:
+                    $IGEM += 1;
+                    break;
+                case 6:
+                    $IIAS += 1;
+                    break;
+                case 7:
+                    $IIND += 1;
+                    break;
+                case 8:
+                    $COP += 1;
+                    break;
+            }
+        }
+
+        $carrersArrayValues = array($ISIC, $IINF, $IBQA, $ICIV, $IGEM, $IIAS, $IIND, $COP);
+
+        $maxValue = max($carrersArrayValues);
+
+        $length = count($carrersArrayValues);
+
+        for ($i = 0; $i<$length; $i++){
+            if($maxValue == $carrersArrayValues[$i]){
+                $maxValuePosition = $i+1;
+            }
+        }
+
+        $carrer = Carrer::select('alias')
+            ->where('id', $maxValuePosition)
+            ->get();
+
+        $data = array($carrer[0]->alias, $maxValue, $maxValuePosition);
+
+        return $data;
+    }
+
+    public function getProfesorWithMoreFaultsByCarrer(){
+
+        $carrerId = $this->getCarrerWithMoreFaults();
+
+        $profesors = Subject::select('profesor_id')
+            ->where('carrer_id', $carrerId[2])
+            ->get();
+
+        $profesor = Profesor::select()
+            ->where('id', $profesors[0]->profesor_id)
+            ->get();
+
+        return $profesor;
+    }
 }
--- resources/views/statistics.blade.php
+++ resources/views/statistics.blade.php
@@ -10,13 +10,15 @@
 
             <div class="col-md-8">
 
-                <div class="row blue">
+                <div class="row">
                     <div class="col-md-4">
                         <div class="card padding-card">
 
                             faltas{{$faults}}
                             /
                             registros{{$totalRowsInNoAssistance}}
+
+                            faltas / assistencias
 
                         </div>
                     </div>
@@ -36,21 +38,48 @@
                     <div class="col-md-4">
                         <div class="card padding-card">
 
+                            {{$totalGroupsWithFaults}} / {{$totalGroups}}
+
                         </div>
                     </div>
                 </div>
 
-                <div class="row margin-top-card red">
+                <div class="row margin-top-card">
 
                     <div class="col-md-12">
                         <div class="card padding-card">
+
+                            <div class="row">
+
+                                <div class="col-md-8 blue">
+                                    Grafica
+                                </div>
+
+                                <div class="col-md-4">
+
+                                    <h6 class="margin-bottom-10px"><b>{{$carrerWithMoreFaults[0]}}</b> es la carrea con más faltas con {{$carrerWithMoreFaults[1]}}.</h6>
+
+                                    <h6 class="margin-bottom-10px">Los días con mas faltas son los <b>lunes</b>.</h6>
+
+                                    <h6 class="margin-bottom-13px">
+                                        Profesor con más faltas:
+                                        <b>{{$profesorWithMoreFaultsByCarrer[0]->first_name}}
+                                            {{$profesorWithMoreFaultsByCarrer[0]->last_name}}
+                                            {{$profesorWithMoreFaultsByCarrer[0]->last_name2}}</b>.
+                                    </h6>
+
+                                    <button type="button" class="btn btn-outline-primary btn-block">GENERAR REPORTE</button>
+
+                                </div>
+
+                            </div>
 
                         </div>
                     </div>
 
                 </div>
                 
-                <div class="row margin-top-card blue">
+                <div class="row margin-top-card">
 
                     <div class="col-md-6">
                         <div class="card padding-card">
