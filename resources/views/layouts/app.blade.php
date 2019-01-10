73c6654186596efdce686ebc05fc5ca56302ec6a
Jose Luis <joselopez@anzen.com.mx>
2019-01-10 15:41:55 -0600

    The view statics finished

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,83 +26,10 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('ITSA Check', 'ITSA Check') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Iniciar sesión') }}</a>
-                            </li>
-                            <li class="nav-item">
-                                @if (Route::has('register'))
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Registrarme') }}</a>
-                                @endif
-                            </li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->first_name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Cerrar sesión') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
 
         <main class="py-4">
             @yield('content')
         </main>
     </div>
-
-    <script>
-
-        var ctx = document.getElementById('myChart').getContext('2d');
-
-        var myBarChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: ["ISIC", "IINF", "IBQA", "ICIV", "IGEM", "IIAS", "IIND", "COP"],
-                datasets: [{
-                    label: "Faltas por carrera",
-                    backgroundColor: ["rgba(255, 99, 132, 0.2)","rgba(201, 203, 207, 0.2)","rgba(255, 205, 86, 0.2)","rgba(75, 192, 192, 0.2)","rgba(54, 162, 235, 0.2)","rgba(153, 102, 255, 0.2)","rgba(152, 156, 164, 0.53)","rgba(255, 159, 64, 0.2)"],
-                    borderColor:["rgb(255, 99, 132)","rgb(201, 203, 207)","rgb(255, 205, 86)","rgb(75, 192, 192)","rgb(54, 162, 235)","rgb(153, 102, 255)","rgb(201, 203, 207)", "rgb(255, 159, 64)"],
-                    borderWidth:1,
-                    data: ["{{$faultsOfCarrers[0]}}", "{{$faultsOfCarrers[1]}}", "{{$faultsOfCarrers[2]}}", "{{$faultsOfCarrers[3]}}", "{{$faultsOfCarrers[4]}}", "{{$faultsOfCarrers[5]}}", "{{$faultsOfCarrers[6]}}", "{{$faultsOfCarrers[7]}}"],
-                }]
-            },
-            options: {}
-        });
-
-    </script>
-
-
 </body>
 </html>
--- resources/views/statistics.blade.php
+++ resources/views/statistics.blade.php
@@ -41,7 +41,7 @@
 
                             <h1 class="text-center">{{$totalGroupsWithFaults}} / {{$totalGroups}}</h1>
 
-                            <h6><b>{{$totalGroupsWithFaults}}</b> grupos de <b>{{$totalGroups}}</b> no tuvieron clase</h6>
+                            <h6><b>{{$totalGroupsWithFaults}}</b> grupos de <b>{{$totalGroups}}</b> no tuvieron clas</h6>
 
                         </div>
                     </div>
@@ -56,13 +56,13 @@
 
                                 <div class="col-md-8">
 
-                                    <canvas id="myChart">{{$faultsOfCarrers[5]}}</canvas>
+                                    <canvas id="statics_carrers"></canvas>
 
                                 </div>
 
                                 <div class="col-md-4">
 
-                                    <h6 class="margin-bottom-10px"><b>{{$carrerWithMoreFaults[0]}}</b> es la carrea con más faltas con {{$carrerWithMoreFaults[1]}}.</h6>
+                                    <h6 class="margin-bottom-10px margin-top-card"><b>{{$carrerWithMoreFaults[0]}}</b> es la carrea con más faltas con {{$carrerWithMoreFaults[1]}}.</h6>
 
                                     <h6 class="margin-bottom-10px">Los días con mas faltas son los <b>lunes</b>.</h6>
 
@@ -73,7 +73,7 @@
                                             {{$profesorWithMoreFaultsByCarrer[0]->last_name2}}</b>.
                                     </h6>
 
-                                    <button type="button" class="btn btn-outline-primary btn-block">GENERAR REPORTE</button>
+                                    <button type="button" class="btn btn-outline-primary btn-block margin-top-85px">GENERAR REPORTE</button>
 
                                 </div>
 
@@ -89,6 +89,15 @@
                     <div class="col-md-6">
                         <div class="card padding-card">
 
+                            <div class="row">
+
+                                <div class="col-md-12">
+                                    <h5>Faltas por genero</h5>
+                                    <canvas id="statics_genders" style="height: 200px;"></canvas>
+                                </div>
+
+                            </div>
+
                         </div>
                     </div>
 
@@ -97,20 +106,20 @@
 
                             <h5>Estadístca general</h5>
 
-                            <div class="row margin-top-10px">
+                            <div class="row margin-top-10px margin-bottom-50px">
 
                                 <div class="col-md-5">
                                     <h6>Grupos: <b>{{$totalGroups}}</b></h6>
-                                    <h6>Profesores: <b>{{$totalProfesors}}</b></h6>
-                                    <h6>Materias: <b>{{$totalSubjects}}</b></h6>
-                                    <h6>Checadores: <b>{{$totalCheckers}}</b></h6>
+                                    <h6 class="margin-top-15px">Profesores: <b>{{$totalProfesors}}</b></h6>
+                                    <h6 class="margin-top-15px">Materias: <b>{{$totalSubjects}}</b></h6>
+                                    <h6 class="margin-top-15px">Checadores: <b>{{$totalCheckers}}</b></h6>
                                 </div>
                                 <div class="col-md-7">
                                     <h6>El promedio de faltas del semestre es:</h6>
 
-                                    <h2 class="text-center"><b>{{$percentageOfFaults}} %</b></h2>
+                                    <h2 class="text-center margin-top-15px"><b>{{$percentageOfFaults}} %</b></h2>
 
-                                    <h6 class="text-center"><b>4</b> faltas por día.</h6>
+                                    <h6 class="text-center margin-top-15px"><b>4</b> faltas por día.</h6>
                                 </div>
 
                             </div>
@@ -125,5 +134,40 @@
         </div>
 
     </div>
+
+    <script>
+
+        var ctxStaticsCarrers = document.getElementById('statics_carrers').getContext('2d');
+
+        var ctxStaticsGenders = document.getElementById('statics_genders').getContext('2d');
+
+        var myBarChart = new Chart(ctxStaticsCarrers, {
+            type: 'bar',
+            data: {
+                labels: ["ISIC", "IINF", "IBQA", "ICIV", "IGEM", "IIAS", "IIND", "COP"],
+                datasets: [{
+                    label: "Faltas por carrera",
+                    backgroundColor: ["rgba(255, 99, 132, 0.2)","rgba(201, 203, 207, 0.2)","rgba(255, 205, 86, 0.2)","rgba(75, 192, 192, 0.2)","rgba(54, 162, 235, 0.2)","rgba(153, 102, 255, 0.2)","rgba(152, 156, 164, 0.53)","rgba(255, 159, 64, 0.2)"],
+                    borderColor:["rgb(255, 99, 132)","rgb(201, 203, 207)","rgb(255, 205, 86)","rgb(75, 192, 192)","rgb(54, 162, 235)","rgb(153, 102, 255)","rgb(201, 203, 207)", "rgb(255, 159, 64)"],
+                    borderWidth:1,
+                    data: ["{{$faultsOfCarrers[0]}}", "{{$faultsOfCarrers[1]}}", "{{$faultsOfCarrers[2]}}", "{{$faultsOfCarrers[3]}}", "{{$faultsOfCarrers[4]}}", "{{$faultsOfCarrers[5]}}", "{{$faultsOfCarrers[6]}}", "{{$faultsOfCarrers[7]}}"],
+                }]
+            },
+            options: {}
+        });
+
+        var myDoughnutChart = new Chart(ctxStaticsGenders, {
+            type: 'doughnut',
+            data: {
+                labels: ["Hombres","Mujeres"],
+                datasets: [{
+                    backgroundColor: ["rgb(54, 162, 235)","rgb(255, 99, 132)"],
+                    data: ["57","43"]
+                }]
+            },
+            options: {}
+        });
+
+    </script>
 
 @endsection
